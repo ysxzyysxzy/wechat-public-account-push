8cb9e0855dd6b6a844e1894bbf20255aa334876a
ysxzyysxzy <114325463+ysxzyysxzy@users.noreply.github.com>
2022-09-25 17:42:56 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,12 +7,12 @@ const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: 'wx950c9f740967f3d9',
+  APP_ID: "wx950c9f740967f3d9",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: 'a2e6b9e6c69f4a18cb1dc95c894e666c',
+  APP_SECRET: "a2e6b9e6c69f4a18cb1dc95c894e666c",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -60,16 +60,16 @@ const USER_CONFIG = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [o15MX6OYmd5FuewtCxg-9nV0Fr0U]
+  USERS: ["o15MX6OYmd5FuewtCxg-9nV0Fr0U"]
     
       // 想要发送的人的名字
       name: '宝贝',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '		lQcenlVB5vls1qW64ynqp3j0mH6nHWSpN8KfkkVUUgw',
+      useTemplateId:"tvWfGKBPaqMiqgHPrBz852nHlThsZHiIZLTqV-AmZdA",
       // 所在省份
-      province: '重庆',
+      province: "重庆",
       // 所在城市
       city: '',
       // 新历生日, 仅用作获取星座运势, 格式必须
